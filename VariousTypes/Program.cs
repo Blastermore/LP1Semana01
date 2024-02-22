1619187fe031c18e17033a01e589f23adabfe724
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-22 19:57:41 +0000

    Added char variables for letters 'd' , 'E' and star symbol

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -11,7 +11,12 @@ namespace VariousTypes
             int b = 0x2;
             uint c = 3U;
 
+            char d = '\u0064';
+            char e = '\u0045';
+            char f = '\u2606';
+
             Console.WriteLine(a +"\n" + b + "\n" + c);
+            Console.WriteLine(d +"\n" + e + "\n" + f);
         }
     }
 }
