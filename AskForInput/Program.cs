a5b96cf4a7b71943b76f6182dfff67c4d02b48ed
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-22 23:01:39 +0000

    Added request for input for a float and attributed it to a variable, converting it to a true float at the end

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -7,9 +7,14 @@ namespace AskForInput
         static void Main(string[] args)
         {
             Console.WriteLine("Insira um número inteiro:");
-            string input = Console.ReadLine();
-            int n = Convert.ToInt16(input);
+            string input_int = Console.ReadLine();
+            int n = Convert.ToInt16(input_int);
 
+            Console.WriteLine("Insira um número real:");
+            string input_real = Console.ReadLine();
+            float r = float.Parse(input_real);
+
+            Console.WriteLine(r);
         }
     }
 }
