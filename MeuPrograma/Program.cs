ed1fde7093a9311e49c9b6f8a0e5b036f3aa5981
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-22 17:40:16 +0000

    Added a summary for the Program class and its Main function

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,6 +2,11 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Inside the namespace is defined 
+    /// the Main function that prints "Hello, 
+    /// World!" and then "Bye world"
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
