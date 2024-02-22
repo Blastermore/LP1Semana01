16b5d637928f2904a0d5309b04ea85050fd6d651
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-22 21:08:48 +0000

    Added some other strings  formed by interpolation or concatenating other strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ManyStrings
 {
@@ -6,16 +7,22 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+            int x = 3;
             string message1 = "Heyo!";
             string message2 = "Wassup!";
             string message3 = "Howdy! \u02C3\u006F\u02C2";
             string message4 = "cool hat!";
+            string message5 = message1 + " How you doin?";
+            string message6 = "I have " + x + " apples!";
+            string message7 = $"I do love having {x} apples!";
 
             Console.WriteLine(message1);
             Console.WriteLine(message2);
             Console.WriteLine(message3);
             Console.WriteLine(message4);
-
+            Console.WriteLine(message5);
+            Console.WriteLine(message6);
+            Console.WriteLine(message7);
 
         }
     }
