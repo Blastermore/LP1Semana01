612ac89096139f80b35f68e36db33815fcae8697
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-22 20:24:25 +0000

    Added some strings and prints of them

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message1 = "Heyo!";
+            string message2 = "Wassup!";
+            string message3 = "Howdy! \u02F2 \u2F3 \u02F1";
+            string message4 = "\u0023 cool"
+
+            Console.WriteLine(message1)
+            Console.WriteLine(message2)
+            Console.WriteLine(message3)
+            Console.WriteLine(message4)
+
+
+        }
+    }
+}
