88a440658290873af232b8af51821240133db3d0
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-02-22 22:51:56 +0000

    Added request for input of a int and subsequent attribution of it to a variable

---
+++ AskForInput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro:");
+            string input = Console.ReadLine();
+            int n = Convert.ToInt16(input);
+
+        }
+    }
+}
